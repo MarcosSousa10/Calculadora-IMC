bd98522045651c4e0fc35798e149b9095cc0112c
Sr-Marcos <marcospegodesousa10@gmail.com>
2022-10-06 07:25:44 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,6 @@
+let calculo =()=>{
+let peso = document.getElementById('peso').value;
+let altura= document.getElementById('altura').value;
+let cal= peso/(altura*altura);
+ let re=document.getElementById('resultado').innerHTML=cal; 
+}
